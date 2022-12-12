98addf7378c051b65ae9423102693c4b23076027
Sanya-27 <112626199+Sanya-27@users.noreply.github.com>
2022-12-12 22:16:28 +0530

    Create first_last_index.py

---
+++ 11_12_22/first_last_index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l=[]
+        if target not in nums:
+            return [-1,-1]
+        for i in range(len(nums)):
+            if nums[i]==target:
+                l.append(i) 
+        l2=[]
+        l2.append(l[0])
+        l2.append(l[-1])
+        return l2
